c3c66197962c779d675df8baa2f212af3b85357f
TharinduDR <rhtdranasinghe@gmail.com>
2018-12-11 14:27:42 +0000

    014: Added ELMo Embedding comparisons

--- matrices/context_vectors/avg_embeddings.py
+++ matrices/context_vectors/avg_embeddings.py
@@ -1,4 +1,5 @@
 import math
+import os
 from collections import Counter
 
 import numpy as np
@@ -6,37 +7,28 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from sklearn.metrics.pairwise import cosine_similarity
 
-from utility.progress_bar import update_progress_bar
+from utility.hashing import convert_to_number
 
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
 
 
-def run_avg_elmo_benchmark(sentences1, sentences2, doc_freqs=None, use_stoplist=False):
+def run_avg_elmo_benchmark(sentences1, sentences2, model_path, doc_freqs=None, use_stoplist=False, ):
     sims = []
 
-    max_length = max(len(sentences1), len(sentences2))
-
-    update_progress_bar(0, max_length, prefix='Progress:', suffix='Complete', length=50)
     counter = 0
     for (sent1, sent2) in zip(sentences1, sentences2):
-        sims.append(get_similarity(sent1, sent2, doc_freqs, use_stoplist))
-        update_progress_bar(counter, max_length, prefix='Progress:', suffix='Complete', length=100)
+        sims.append(get_similarity(sent1, sent2, model_path, doc_freqs, use_stoplist))
         counter = counter + 1
 
     return sims
 
 
-def get_similarity(sent1, sent2, doc_freqs=None, use_stoplist=False):
-    init = tf.initialize_all_variables()
-    sess = tf.Session()
-    sess.run(init)
-    tf.logging.set_verbosity(tf.logging.ERROR)
-
+def get_similarity(sent1, sent2, model_path, doc_freqs=None, use_stoplist=False):
     if doc_freqs is not None:
         N = doc_freqs["NUM_DOCS"]
 
-    tokens1 = sent1.tokens_without_stop if use_stoplist else sent1.tokens
-    tokens2 = sent2.tokens_without_stop if use_stoplist else sent2.tokens
+    tokens1 = sent1.tokens
+    tokens2 = sent2.tokens
 
     tokfreqs1 = Counter(tokens1)
     tokfreqs2 = Counter(tokens2)
@@ -47,20 +39,85 @@ def get_similarity(sent1, sent2, doc_freqs=None, use_stoplist=False):
     weights2 = [tokfreqs2[token] * math.log(N / (doc_freqs.get(token, 0) + 1))
                 for token in tokens2] if doc_freqs else None
 
-    embeddings1 = elmo(inputs={"tokens": [tokens1], "sequence_len": [len(tokens1)]}, signature="tokens", as_dict=True)[
-        "elmo"]
+    file_name1 = str(convert_to_number(sent1.raw))
+    file_name2 = str(convert_to_number(sent2.raw))
 
-    embeddings2 = elmo(inputs={"tokens": [tokens2], "sequence_len": [len(tokens2)]}, signature="tokens", as_dict=True)[
-        "elmo"]
+    if not os.path.isfile(os.path.join(model_path, '1', file_name1 + '.npy')):
 
-    embedding1 = np.average([embedding for embedding in sess.run(embeddings1[0])], axis=0,
-                            weights=weights1).reshape(1, -1)
+        init = tf.initialize_all_variables()
+        sess = tf.Session()
+        sess.run(init)
+        tf.logging.set_verbosity(tf.logging.ERROR)
 
-    embedding2 = np.average([embedding for embedding in sess.run(embeddings2[0])], axis=0,
-                            weights=weights2).reshape(1, -1)
+        embeddings1 = \
+            elmo(inputs={"tokens": [tokens1], "sequence_len": [len(tokens1)]}, signature="tokens", as_dict=True)[
+                "elmo"]
+        embedding1 = sess.run(embeddings1[0])
+
+        sess.close()
+
+    else:
+        embedding1 = np.load(os.path.join(model_path, '1', file_name1 + '.npy'))
+
+    if not os.path.isfile(os.path.join(model_path, '2', file_name2 + '.npy')):
+
+        init = tf.initialize_all_variables()
+        sess = tf.Session()
+        sess.run(init)
+        tf.logging.set_verbosity(tf.logging.ERROR)
+
+        embeddings2 = \
+            elmo(inputs={"tokens": [tokens2], "sequence_len": [len(tokens2)]}, signature="tokens", as_dict=True)[
+                "elmo"]
+        embedding2 = sess.run(embeddings2[0])
+
+        sess.close()
+
+    else:
+        embedding2 = np.load(os.path.join(model_path, '2', file_name2 + '.npy'))
+
+    related_tokens1 = sent1.tokens_without_stop if use_stoplist else sent1.tokens
+    related_tokens2 = sent2.tokens_without_stop if use_stoplist else sent2.tokens
+
+    related_embeddings1 = []
+    related_embeddings2 = []
+
+    related_weights1 = []
+    related_weights2 = []
+
+    if len(related_tokens1) != len(tokens1):
+        for related_token1 in related_tokens1:
+            index = tokens1.index(related_token1)
+            related_embeddings1.append(embedding1[index])
+            if doc_freqs is not None:
+                related_weights1.append((weights1[index]))
+            else:
+                related_weights1 = None
+
+        embedding1 = np.average([embedding for embedding in related_embeddings1], axis=0,
+                                weights=related_weights1).reshape(1, -1)
+    else:
+        embedding1 = np.average([embedding for embedding in embedding1], axis=0,
+                                weights=weights1).reshape(1, -1)
+
+    if len(related_tokens2) != len(tokens2):
+        for related_token2 in related_tokens2:
+            index = tokens2.index(related_token2)
+            related_embeddings2.append(embedding2[index])
+
+            if doc_freqs is not None:
+                related_weights2.append(weights2[index])
+
+            else:
+                related_weights2 = None
+
+        embedding2 = np.average([embedding for embedding in related_embeddings2], axis=0,
+                                weights=related_weights2).reshape(1, -1)
+
+    else:
+        embedding2 = np.average([embedding for embedding in embedding2], axis=0,
+                                weights=weights2).reshape(1, -1)
 
     sim = cosine_similarity(embedding1, embedding2)[0][0]
-
-    sess.close()
 
     return sim
--- matrices/context_vectors/smooth_inverse_frequency.py
+++ matrices/context_vectors/smooth_inverse_frequency.py
@@ -1,8 +1,12 @@
+import os
+
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
+
+from utility.hashing import convert_to_number
 
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
 
@@ -15,12 +19,7 @@ def remove_first_principal_component(X):
     return XX
 
 
-def run_sif_benchmark(sentences1, sentences2, model, freqs={}, a=0.001):
-    init = tf.initialize_all_variables()
-    sess = tf.Session()
-    sess.run(init)
-    tf.logging.set_verbosity(tf.logging.ERROR)
-
+def run_sif_benchmark(sentences1, sentences2, model_path, freqs={}, a=0.001):
     total_freq = sum(freqs.values())
 
     embeddings = []
@@ -33,6 +32,45 @@ def run_sif_benchmark(sentences1, sentences2, model, freqs={}, a=0.001):
 
         weights1 = [a / (a + freqs.get(token, 0) / total_freq) for token in tokens1]
         weights2 = [a / (a + freqs.get(token, 0) / total_freq) for token in tokens2]
+
+        file_name1 = str(convert_to_number(sent1.raw))
+        file_name2 = str(convert_to_number(sent2.raw))
+
+        if not os.path.isfile(os.path.join(model_path, '1', file_name1 + '.npy')):
+
+            init = tf.initialize_all_variables()
+            sess = tf.Session()
+            sess.run(init)
+            tf.logging.set_verbosity(tf.logging.ERROR)
+
+            embeddings1 = \
+                elmo(inputs={"tokens": [tokens1], "sequence_len": [len(tokens1)]}, signature="tokens", as_dict=True)[
+                    "elmo"]
+            embedding1 = sess.run(embeddings1[0])
+
+            sess.close()
+
+        else:
+            embedding1 = np.load(os.path.join(model_path, '1', file_name1 + '.npy'))
+
+        if not os.path.isfile(os.path.join(model_path, '2', file_name2 + '.npy')):
+
+            init = tf.initialize_all_variables()
+            sess = tf.Session()
+            sess.run(init)
+            tf.logging.set_verbosity(tf.logging.ERROR)
+
+            embeddings2 = \
+                elmo(inputs={"tokens": [tokens2], "sequence_len": [len(tokens2)]}, signature="tokens", as_dict=True)[
+                    "elmo"]
+            embedding2 = sess.run(embeddings2[0])
+
+            sess.close()
+
+        else:
+            embedding2 = np.load(os.path.join(model_path, '2', file_name2 + '.npy'))
+
+
 
         embedding1 = np.average([model[token] for token in tokens1], axis=0, weights=weights1)
         embedding2 = np.average([model[token] for token in tokens2], axis=0, weights=weights2)
