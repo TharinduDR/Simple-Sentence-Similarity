23a2851bc00ace9fd2b254ee268f9c229bfdca00
TharinduDR <rhtdranasinghe@gmail.com>
2020-09-14 19:37:42 +0100

    018: Adding more experiments

--- matrices/context_vectors/avg_embeddings.py
+++ matrices/context_vectors/avg_embeddings.py
@@ -49,8 +49,8 @@ def run_context_avg_benchmark(sentences1, sentences2, model=None, use_stoplist=F
         # flair_sent1 = Sentence(" ".join(flair_tokens1))
         # flair_sent2 = Sentence(" ".join(flair_tokens2))
 
-        # print("sent_1 is " + (" ".join(flair_tokens1)))
-        # print("sent_2 is " + (" ".join(flair_tokens2)))
+        print("sent_1 is " + (" ".join(tokens1)))
+        print("sent_2 is " + (" ".join(tokens2)))
 
         # model.embed(flair_sent1)
         # model.embed(flair_sent2)
@@ -78,6 +78,9 @@ def run_context_avg_benchmark(sentences1, sentences2, model=None, use_stoplist=F
 
         embedding1 = np.average([embeddings_map1[token] for token in tokfreqs1 if token in embeddings_map1], axis=0, weights=weights1).reshape(1, -1)
         embedding2 = np.average([embeddings_map2[token] for token in tokfreqs2 if token in embeddings_map2], axis=0, weights=weights2).reshape(1, -1)
+
+        print(embedding1)
+        print(embedding2)
 
         sim = cosine_similarity(embedding1, embedding2)[0][0]
         sims.append(sim)
