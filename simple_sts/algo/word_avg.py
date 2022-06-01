e29d0f3f0f310482c9813de1f8bb2a4b0d4d5f9e
TharinduDR <rhtdranasinghe@gmail.com>
2022-06-01 14:28:12 +0100

    020: Project restructuring

--- simple_sts/algo/word_avg.py
+++ simple_sts/algo/word_avg.py
@@ -68,7 +68,6 @@ class WordEmbeddingAverageSTSMethod:
                               len(processed_sentences_1 + processed_sentences_2) % batch_size > 0), desc="Embedding sentences "):
             self.embedding_model.embed(x)
 
-        print(self.model_args.remove_stopwords)
         for embed_sentence_1, embed_sentence_2 in tqdm(zip(processed_sentences_1, processed_sentences_2), total=len(processed_sentences_1), desc="Calculating similarity "):
 
             embedding1 = np.average([np.array(token1.embedding.data.tolist()) for token1 in embed_sentence_1 if self.model_args.remove_stopwords and token1.text not in self.stop_words], axis=0)
