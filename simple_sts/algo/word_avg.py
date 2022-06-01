9b9f699c4651bc815a7e5d8a39b1b77adf09472c
TharinduDR <rhtdranasinghe@gmail.com>
2022-06-01 10:57:23 +0100

    020: Project restructuring

--- simple_sts/algo/word_avg.py
+++ simple_sts/algo/word_avg.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from simple_sts.model_args import WordEmbeddingSTSArgs
 from simple_sts.util import batch
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -61,6 +62,7 @@ class WordEmbeddingAverageSTSMethod:
             self.embedding_model.embed(x)
 
         for embed_sentence_1, embed_sentence_2 in tqdm(zip(processed_sentences_1, processed_sentences_2), total=len(processed_sentences_1), desc="Calculating similarity "):
+
             embedding1 = np.average([np.array(token1.embedding.data.tolist()) for token1 in embed_sentence_1], axis=0)
             embedding2 = np.average([np.array(token2.embedding.data.tolist()) for token2 in embed_sentence_2], axis=0)
             cos_sim = dot(embedding1, embedding2) / (norm(embedding1) * norm(embedding2))
