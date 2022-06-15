da77c7e1e9ff30644e54d15e3b2c891fcd1b047f
TharinduDR <rhtdranasinghe@gmail.com>
2022-06-15 23:01:07 +0100

    020: Project restructuring

--- examples/english/sick/run_experiment.py
+++ examples/english/sick/run_experiment.py
@@ -3,6 +3,7 @@ import pandas as pd
 from examples.evaluation import pearson_corr, spearman_corr, rmse
 from simple_sts.algo.cls import TransformerCLSSTSMethod
 from simple_sts.algo.labse import LaBSESTSMethod
+from simple_sts.algo.laser import LASERSTSMethod
 from simple_sts.algo.sbert import SentenceTransformerSTSMethod
 from simple_sts.algo.sif import WordEmbeddingSIFSTSMethod
 from simple_sts.algo.use import UniversalSentenceEncoderSTSMethod
@@ -19,40 +20,40 @@ for index, row in sick_test.iterrows():
     to_predit.append([row['sentence_A'], row['sentence_B']])
     sims.append(row['relatedness_score'])
 
-# model_args = WordEmbeddingSTSArgs()
-# model_args.embedding_models = [["transformer", "bert-base-multilingual-cased"]]
-# model_args.language = "en"
-# model_args.remove_stopwords = True
-#
-# # model = WordEmbeddingAverageSTSMethod(model_args=model_args)
-# model = WordEmbeddingSIFSTSMethod(model_args=model_args)
-#
-# pred_sims = model.predict(to_predit)
-# print("Pearson correlation ", pearson_corr(sims, pred_sims))
-# print("Spearman correlation ", spearman_corr(sims, pred_sims))
-# print("RMSE ", rmse(sims, pred_sims))
-#
-# # -----------------------------------------------------------------------
-#
-# sentence_model_args = SentenceEmbeddingSTSArgs()
-# sentence_model_args.embedding_model = "https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3"
-# sentence_model_args.language = "en"
-#
-#
-# # model = WordEmbeddingAverageSTSMethod(model_args=model_args)
-# model = UniversalSentenceEncoderSTSMethod(model_args=sentence_model_args)
-#
-# pred_sims = model.predict(to_predit)
-# print("Pearson correlation ", pearson_corr(sims, pred_sims))
-# print("Spearman correlation ", spearman_corr(sims, pred_sims))
-# print("RMSE ", rmse(sims, pred_sims))
+model_args = WordEmbeddingSTSArgs()
+model_args.embedding_models = [["transformer", "bert-base-multilingual-cased"]]
+model_args.language = "en"
+model_args.remove_stopwords = True
+
+# model = WordEmbeddingAverageSTSMethod(model_args=model_args)
+model = WordEmbeddingSIFSTSMethod(model_args=model_args)
+
+pred_sims = model.predict(to_predit)
+print("Pearson correlation ", pearson_corr(sims, pred_sims))
+print("Spearman correlation ", spearman_corr(sims, pred_sims))
+print("RMSE ", rmse(sims, pred_sims))
+
+# -----------------------------------------------------------------------
+
+sentence_model_args = SentenceEmbeddingSTSArgs()
+sentence_model_args.embedding_model = "https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3"
+sentence_model_args.language = "en"
+
+
+# model = WordEmbeddingAverageSTSMethod(model_args=model_args)
+model = UniversalSentenceEncoderSTSMethod(model_args=sentence_model_args)
+
+pred_sims = model.predict(to_predit)
+print("Pearson correlation ", pearson_corr(sims, pred_sims))
+print("Spearman correlation ", spearman_corr(sims, pred_sims))
+print("RMSE ", rmse(sims, pred_sims))
 
 # -----------------------------------------------------------------------
 # labse_model_args = SentenceEmbeddingSTSArgs()
 # labse_model_args.embedding_model = "https://tfhub.dev/google/LaBSE/2"
 # labse_model_args.language = "en"
-
-
+#
+#
 # model = WordEmbeddingAverageSTSMethod(model_args=model_args)
 # model = LaBSESTSMethod(model_args=labse_model_args)
 #
@@ -83,6 +84,19 @@ cls_model_args.language = "en"
 
 # model = WordEmbeddingAverageSTSMethod(model_args=model_args)
 model = TransformerCLSSTSMethod(model_args=cls_model_args)
+
+pred_sims = model.predict(to_predit)
+print("Pearson correlation ", pearson_corr(sims, pred_sims))
+print("Spearman correlation ", spearman_corr(sims, pred_sims))
+print("RMSE ", rmse(sims, pred_sims))
+
+# -----------------------------------------------------------------------
+laser_model_args = SentenceEmbeddingSTSArgs()
+laser_model_args.language = "en"
+
+
+# model = WordEmbeddingAverageSTSMethod(model_args=model_args)
+model = LASERSTSMethod(model_args=laser_model_args)
 
 pred_sims = model.predict(to_predit)
 print("Pearson correlation ", pearson_corr(sims, pred_sims))
---
+++ simple_sts/algo/laser.py
@@ -0,0 +1,70 @@
+import logging
+import os
+import sys
+import urllib.request
+from laserembeddings import Laser
+from tqdm import tqdm
+from numpy.linalg import norm
+import numpy as np
+from simple_sts.util import batch
+
+from simple_sts.model_args import SentenceEmbeddingSTSArgs
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def download_file(url, dest):
+    sys.stdout.flush()
+    urllib.request.urlretrieve(url, dest)
+
+
+def download_models(output_dir):
+    logger.info('Downloading models into {}', output_dir)
+
+    download_file('https://dl.fbaipublicfiles.com/laser/models/93langs.fcodes',
+                  os.path.join(output_dir, '93langs.fcodes'))
+    download_file('https://dl.fbaipublicfiles.com/laser/models/93langs.fvocab',
+                  os.path.join(output_dir, '93langs.fvocab'))
+    download_file(
+        'https://dl.fbaipublicfiles.com/laser/models/bilstm.93langs.2018-12-26.pt',
+        os.path.join(output_dir, 'bilstm.93langs.2018-12-26.pt'))
+
+
+class LASERSTSMethod:
+    def __init__(self, model_args: SentenceEmbeddingSTSArgs):
+        self.model_args = model_args
+        logging.info("Loading models ")
+
+        output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+        download_models(output_dir)
+        self.embedding_model = Laser()
+
+    def predict(self, to_predict, batch_size=32):
+        sims = []
+
+        sentences_1 = list(zip(*to_predict))[0]
+        sentences_2 = list(zip(*to_predict))[1]
+
+        embeddings_1 = []
+        embeddings_2 = []
+        sims = []
+
+        for x in tqdm(batch(sentences_1, batch_size), total=int(len(sentences_1) / batch_size) + (
+                len(sentences_1) % batch_size > 0), desc="Embedding list 1 "):
+            temp = self.embedding_model.embed_sentences(x, lang='en')
+            for embedding in temp:
+                embeddings_1.append(embedding)
+
+        for x in tqdm(batch(sentences_2, batch_size), total=int(len(sentences_2) / batch_size) + (
+                len(sentences_2) % batch_size > 0), desc="Embedding list 2 "):
+            temp = self.embedding_model.embed_sentences(x, lang='en')
+            for embedding in temp:
+                embeddings_2.append(embedding)
+
+        for embedding_1, embedding_2 in tqdm(zip(embeddings_1, embeddings_2), total=len(embeddings_1)):
+            cos_sim = np.dot(embedding_1, embedding_2) / (
+                    norm(embedding_1) * norm(embedding_2))
+            sims.append(cos_sim)
+
+        return sims
