c4d90cf6d89a6e7998a99415db69acfb6eb2807d
TharinduDR <rhtdranasinghe@gmail.com>
2022-06-16 15:32:46 +0100

    020: Project restructuring

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple_sts",
+    version="2.0.0",
+    author="Tharindu Ranasinghe",
+    author_email="rhtdranasinghe@gmail.com",
+    description="Unsupervised models for Semantic Textual Similarity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TharinduDR/Simple-Sentence-Similarity",
+    packages=find_packages(exclude=("examples", )),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas"
+        "numpy"
+        "flair"
+        "allennlp"
+        "tqdm"
+        "pyemd"
+        "stop_words"
+        "tensorflow_text"
+        "tensorflow_hub"
+        "sentence_transformers"
+        "laserembeddings"
+    ],
+)
